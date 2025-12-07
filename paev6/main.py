f194b27dc119e93108585018673480ec7b704b92
Your Name <34074906+MM1132@users.noreply.github.com>
2025-12-07 16:56:40 +0100

    6. päeva Regex lahendus

--- paev6/main.py
+++ paev6/main.py
@@ -1,29 +1,29 @@
 import re
 import math
 
-with open("input.txt") as f:
-	line = f.readlines()[0].strip()
+with open("test.txt") as f:
+	rida = f.readlines()[0].strip()
 
 last_was_upper = False
-length = 0
-start_index = 0
-longest_found_start = 0
-longest_found_length = 0
-for i, char in enumerate(line):
+pikkus = 0
+praegune_algus = 0
+pikima_algus = 0
+pikim_pikkus = 0
+for i, taht in enumerate(rida):
 	if i == 0:
-		last_was_upper = char.isupper()
+		last_was_upper = taht.isupper()
 		continue
 
 	# Then it's all good! 
-	if char.isupper() != last_was_upper:
-		length += 1
-		if length > longest_found_length:
-			longest_found_length = length
-			longest_found_start = start_index
-		last_was_upper = char.isupper()
-	elif char.isupper() == last_was_upper:
-		start_index = i
-		length = 0
+	if taht.isupper() != last_was_upper:
+		pikkus += 1
+		if pikkus > pikim_pikkus:
+			pikim_pikkus = pikkus
+			pikima_algus = praegune_algus
+		last_was_upper = taht.isupper()
+	elif taht.isupper() == last_was_upper:
+		praegune_algus = i
+		pikkus = 0
 
-print(longest_found_start, longest_found_length)
-print(line[longest_found_start:longest_found_start + longest_found_length + 1])
+print(pikima_algus, pikim_pikkus)
+print(rida[pikima_algus:pikima_algus + pikim_pikkus + 1])
--- paev6/regex_solution.py
+++ paev6/regex_solution.py
@@ -4,9 +4,12 @@ from functools import reduce
 with open("test.txt") as f:
 	line = f.readline().strip()
 
-mat = re.finditer(r'([a-z][A-Z])+|([A-Z][a-z])+', line)
+mat = re.finditer(r'([a-z][A-Z])+[a-z]?|([A-Z][a-z])+[A-Z]?', line)
 
 longest_match = \
 	reduce(lambda a, b: b if len(b.group()) > len(a.group()) else a, mat)
 
 print(longest_match.group())
+
+# for m in mat:
+# 	print(m.group())
